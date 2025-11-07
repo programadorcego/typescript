c47cb094384a68fa20f83c95bcbdb27d5ad762b7
Programador Cego <programadorcego@gmail.com>
2025-11-07 07:32:37 -0300

    Tipagem em objetos

---
+++ src/objetos.ts
@@ -0,0 +1,11 @@
+const pessoa: {
+    nome: string,
+    idade: number,
+    email: string,
+    avatar?: string, // Propriedade opcional
+} = {
+    nome: "willian",
+    idade: 35,
+    email: "programadorcego@gmail.com",
+    avatar: "imagem.jpg"
+}
