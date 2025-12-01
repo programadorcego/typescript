b7eaeb560ee80a5adc36ab955b381df77530be53
Programador Cego <programadorcego@gmail.com>
2025-12-01 06:55:35 -0300

    Tipos undefined e null

---
+++ src/null_undefined.ts
@@ -0,0 +1,12 @@
+// undefined
+let valor: string;
+//console.log(valor);
+
+const pessoa = {
+    nome: "willian"
+}
+//console.log(pessoa.email);
+
+// null
+let variavel = null;
+console.log(variavel);
