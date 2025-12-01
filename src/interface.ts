cab52fb0a0b1017850304541e7f6c02f26bae3a9
Programador Cego <programadorcego@gmail.com>
2025-12-01 07:17:59 -0300

    Interface

---
+++ src/interface.ts
@@ -0,0 +1,17 @@
+interface Produto {
+    nome: string;
+    descricao: string;
+    preco: number;
+}
+
+const produto: Produto = {
+    nome: "Notebook",
+    descricao: "descrição teste",
+    preco: 5000
+}
+
+const produto2: Produto = {
+    nome: "Celular",
+    descricao: "Descrição Teste",
+    preco: 2000,
+}
