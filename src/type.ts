e5ab369de532b71ae315ee7d4fea75fcf706dd45
Programador Cego <programadorcego@gmail.com>
2025-12-29 06:50:25 -0300

    Aula 13: Type

---
+++ src/type.ts
@@ -0,0 +1,25 @@
+type Product = {
+    id: number,
+    name: string,
+    price: number,
+}
+
+const produto: Product = {
+    id: 1,
+    name: "Produto 1",
+    price: 100,
+};
+
+const produtos: Product[] = [
+    {
+        id: 1,
+        name: "Produto 1",
+        price: 100,
+    },
+
+    {
+        id: 2,
+        name: "Produto 2",
+        price: 100,
+    }
+]
